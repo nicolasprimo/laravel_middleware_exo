978fed52023cbb9c625fd80a83e7d7037f3f4127
nicolas <nicolas@molengeek.com>
2020-12-08 15:51:30 +0100

    rajout de la pagination sur les posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 <div class="my-3 p-3 bg-white rounded shadow-sm">
-    <h2 class="border-bottom border-gray pb-2 mb-0">Articles</h2>
+    <h2 class="border-bottom border-gray pb-2 mb-0">Articles @if(Request()->page) page : {{ Request()->page}}@endif</h2>
     @foreach($posts as $post)
     <div class="media text-muted pt-3">
       <!-- <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg> -->
@@ -14,5 +14,7 @@
       </p>
     </div>   
     @endforeach  
+
   </div>
+  {{ $posts->links() }}
 @endsection
